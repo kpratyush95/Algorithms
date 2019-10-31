5749c927c4f99bf7b1f12a3ade51d75732407678
kpratyush95 <kpratyush95@gmail.com>
2019-10-31 12:19:16 -0400

    Add files via upload

---
+++ Sorting/Quick_Sort.py
@@ -0,0 +1,26 @@
+def quick_sort(A,p,r):
+    if p < r:
+        q=partition(A,p,r)
+        quick_sort(A,p,q-1)
+        quick_sort(A,q+1,r)
+
+def partition(A,p,r):
+    x = A[r]
+    i = p-1
+    for j in range(p,r):
+        if A[j]<=x:
+            i = i+1
+            A[i],A[j] =  A[j],A[i]
+
+    A[i+1], A[r] = A[r], A[i+1]
+    return i+1
+
+def main():
+    Array_sorting_required = [643,6547,234,2332,668,4324,6796,234423,97865,2322,54,2,96,8,7,564,64623]
+    #Array_sorting_required = [2, 8, 7, 1, 3, 5, 6, 4]
+    start= 0
+    end = len(Array_sorting_required)
+    quick_sort(Array_sorting_required,start,end-1)
+    print(Array_sorting_required)
+
+main()
